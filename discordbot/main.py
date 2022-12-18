1146a7b23c6290357dacc1f419bdd4686a3873f2
okamos <himinato.k@gmail.com>
2022-12-18 22:42:42 +0900

    feat: add discord bot
    start the minecraft server

---
+++ discordbot/main.py
@@ -0,0 +1,26 @@
+import compute
+
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("mc start"):
+        try:
+            start_instance(os.getenv("GCP_PROJECT"), os.getenv("GCP_ZONE"), os.getenv("GCP_MC_INSTANCE"))
+        except ZeroDivisionError:
+            await message.channel.send("起動に失敗しました”）
+        await message.channel.send("起動しました")
+
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
