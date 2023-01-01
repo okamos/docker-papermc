4b2d75ea0c39a486dd5b5625b4462754128fd2eb
okamos <himinato.k@gmail.com>
2023-01-02 05:11:34 +0900

    feat: check mc server status

--- discordbot/main.py
+++ discordbot/main.py
@@ -1,15 +1,35 @@
-import compute
-
-import discord
 import os
+
+# internal
+import compute
+import mcstatus
+
+# external
+import discord
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+channel = os.getenv("DISCORD_CHANNEL")
+mc_client = mcstatus.Client(os.getenv("MC_HOST"), os.getenv("MC_PORT"), 2)
+mcdown = True
+
+@tasks.loop(seconds=20)
+async def checkmc():
+    global mcdown
+    c = client.get_channel(channel)
+    try:
+        response = mc_client.get_status()
+        print(response)
+        if mcdown is True:
+            await c.send(statusMsg % (":white_check_mark:", "マインクラフトサーバーが開始しました"))
+        mcdown = False
+    except:
+        if mcdown is False:
+            await c.send(statusMsg % (":x:", "マインクラフトサーバーが停止しました"))
+        mcdown = True
 
 @client.event
 async def on_message(message):
@@ -17,11 +37,16 @@ async def on_message(message):
         return
 
     if message.content.startswith("mc start"):
+        await message.channel.send("起動中です")
         try:
             compute.start_instance(os.getenv("GCP_PROJECT"), os.getenv("GCP_ZONE"), os.getenv("GCP_MC_INSTANCE"))
         except:
             await message.channel.send("起動に失敗しました")
             return
-        await message.channel.send("起動しました")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    checkmc.start()
 
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
